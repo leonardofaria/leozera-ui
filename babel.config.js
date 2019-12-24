be51e664959862c8d9add8e977c7fee862111398
Leonardo Faria <leonardofaria@gmail.com>
2019-12-23 23:39:32 -0800

    Remove extra Babel packages

--- babel.config.js
+++ babel.config.js
@@ -18,8 +18,7 @@ module.exports = function (api) {
   }
 
   const plugins = [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
+    // '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-do-expressions',
     '@babel/plugin-proposal-export-default-from',
   ];
